e9a384805ba480294fdc29f56b13e2caa6a1b90a
James Cranwell-Ward <jcranwellward@gmail.com>
2014-09-24 14:40:45 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='Flask-MongoRest',
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/elasticsales/flask-mongorest',
     license='BSD',
     author='Anthony Nemitz',
